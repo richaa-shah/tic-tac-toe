f3e7f3149688ef8f2837cacaa80cae2826a7906c
richaa-shah <16167921+richaa-shah@users.noreply.github.com>
2019-12-18 10:57:29 +0000

    Display voard on page

---
+++ src/Components/Game.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { Component } from "react";
+import Board from "./Board";
+
+export default class Game extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      xIsNext: true,
+      stepNumber: 0,
+      history: [
+        {squares: Array(9).fill(null)}
+      ]
+    }
+  }
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    return (
+      <div className="game">
+      <div className="game-board">
+      <Board onClick={(i)=>this.onClick(i)}
+      squares={current.squares} />
+      </div>
+      </div>
+    )
+  }
+
+
+}
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -3,5 +3,6 @@ import * as React from "react";
 export default function Square(props) {
   return (<button className = "square" onClick={props.onClick}>
   {props.value}
-  </button>;)
+  </button>
+  )
 }
